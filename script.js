bc4251d241468f15ad3efc0f69ec447ceb0151da
mariaassisf <90994184+mariaassisf@users.noreply.github.com>
2021-12-27 20:38:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Olá seja bem vindo!")
+let nome = prompt("qual e o seu nome?")
+let mensagem = ` olá! Seja bem vindo(a) ${nome}! Tudo que esta nessa pagina e 100% baseado em fatos reais, então se gosta de uma boa historia, aproveite!`
+alert(mensagem)
